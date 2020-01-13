5876cb9041d2211f9349ca4aa6ab0093e98a438b
Chris Papadopoulos <xristos8170@gmail.com>
2020-01-13 21:01:35 +0200

    Add files via upload

---
+++ src/warmup/pkg1_monkeytrouble/WarmUp1_MonkeyTrouble.java
@@ -0,0 +1,15 @@
+package warmup.pkg1_monkeytrouble;
+
+public class WarmUp1_MonkeyTrouble {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile){
+        if (aSmile && bSmile){
+            return true;
+        }
+        return !aSmile && !bSmile;
+    }
+}
